6ef116143872804ee5ba4cf8fa625a5569bd9dcb
bodhi-chitta <1sidharthsaini@gmail.com>
2022-03-31 21:57:58 +0530

    third

---
+++ index.js
@@ -0,0 +1,3 @@
+let secBody = document.querySelector('.body');
+secBody.innerText = `Hello This is first note`
+secBody.innerHTML=`<div style=background-color:lightblue>This is new content inside Body</div>`;
